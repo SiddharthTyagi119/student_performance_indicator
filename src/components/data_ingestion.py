d5527b4d783818dd1484c5994cee78a1cfdae822
siddharth Tyagi <sidtyagi05@gmail.com>
2023-03-14 10:40:09 +0530

    training complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer 
+
+
 
 ##definig the path to store data
 #it just providing the input things that are req for data ingestion
@@ -61,7 +65,11 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
 
 
